fa6f9c0650b0652b537f8a9f136a775f892be470
masud309060 <masud306090@gmail.com>
2020-08-24 13:53:36 +0600

    shop page coding done again

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import fakeData from '../../fakeData'
 import './Shop.css'
-import Product from './Product/Product';
+import Product from '../Product/Product';
 import Cart from '../../components/Cart/Cart'
 
 const Shop = () => {
